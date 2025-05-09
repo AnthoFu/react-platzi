cd3122f86650f711213d0c09d125e650da6e4551
Anthony Fuentes <anthony.fuentes2005@gmail.com>
2025-05-09 12:13:20 -0400

    [Clase 6] Manejo de Formulario de name Interactivo

--- react-1/src/App.tsx
+++ react-1/src/App.tsx
@@ -2,26 +2,32 @@ import './App.css'
 import Card from './components/card';
 import Counter from './components/counter'
 import ToggleButton from './components/toggleButton';
+import AnthoFu from './assets/AnthoFu.png'
+import NameForm from './components/nameForm';
 
 function App() {
   const items = ['React', 'TypeScript', 'Vite']
 
   return (
     <section>
+      <img className='logo' src={AnthoFu} alt=""/>
     <h1>¡Hola, Bienvenido al proyecto de react de AnthoFu!</h1>
-    <Card title="Card 1" description="Esta es una descripcion, una 'Prop'"/>
-    <Card title="Card 2" description="Esta es una segunda card"/>
-    <Card title="Card 3" description="Esta es una tercera card"/>
-    <Counter/>
-    <ToggleButton/>
-    <ul>
+
+        <p>Este proyecto utiliza:</p>
+        <ul>
       {
       items.map((item, index) => (
         <li key={index}>{item}</li>
       ))
     }
     </ul>
-      
+
+    <Card title="Card 1" description="Esta es una descripcion, una 'Prop'"/>
+    <Counter/>
+    <ToggleButton/>
+
+    <h2>Formulario de nombre</h2>
+    <NameForm/>
     </section>
   );
 }
