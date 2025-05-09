c53b302aa0674ee6a1cd0d85e4bb31d68ddc5bbe
Anthony Fuentes <anthony.fuentes2005@gmail.com>
2025-05-09 09:59:49 -0400

    [Clase 3] Componentes Reutilizables con Props

--- react-1/src/App.tsx
+++ react-1/src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Card from './components/card';
 
 function App() {
   const items = ['React', 'TypeScript', 'Vite']
@@ -6,6 +7,9 @@ function App() {
   return (
     <section>
     <h1>¡Hola AnthoFu!</h1>
+    <Card title="Card 1" description="Esta es una descripcion, una 'Prop'"/>
+    <Card title="Card 2" description="Esta es una segunda card"/>
+    <Card title="Card 3" description="Esta es una tercera card"/>
     <ul>
       {
       items.map((item, index) => (
