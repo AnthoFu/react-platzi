019ed265d1051c65242c7230870b93836f9a916f
Anthony Fuentes <anthony.fuentes2005@gmail.com>
2025-05-14 11:03:07 -0400

    [Clase 17] Manejo de Estados Globales y Proveedores

--- proyecto-react-3/src/App.tsx
+++ proyecto-react-3/src/App.tsx
@@ -1,11 +1,59 @@
+import { createContext, useContext, useState } from "react"
 import Counter from "./components/counter/counter"
+import './App.css'
 
-function app () {
+type ThemeContextType = {
+  theme: string
+  toggleTheme: () => void
+}
+
+const ThemeContext = createContext<ThemeContextType>({
+  theme: 'dark',
+  toggleTheme: () => {},
+})
+
+type ThemeProviderProps = {
+  children: React.ReactNode
+}
+
+function ThemeProvider({ children }: ThemeProviderProps) {
+  const [theme, setTheme] = useState('dark') 
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === 'dark' ? 'light' : 'dark'))
+  }
+
+  return(
+    <ThemeContext.Provider value={{ theme, toggleTheme }}> 
+      {children}
+    </ThemeContext.Provider>
+  )
+}
+
+function ThemeButton() {
+  const { theme, toggleTheme } = useContext(ThemeContext)
+
+  return(
+    <button
+      onClick={toggleTheme}
+      style={{
+        backgroundColor: theme === 'dark' ? '#333' : '#FFF',
+        color: theme === 'dark' ? '#FFF' : '#000' // Coma agregada
+      }}>
+      Cambiar tema
+    </button>
+  )
+}
+
+function App() { 
   return(
     <div>
-      <Counter/>
+      <ThemeProvider>
+        <Counter/>
+        <ThemeButton/>
+      </ThemeProvider>
     </div>
   )
 }
 
-export default app
+export default App 
